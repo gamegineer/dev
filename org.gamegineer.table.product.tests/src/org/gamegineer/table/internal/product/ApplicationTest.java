c3a50cb276a72b88a87ac3795039b2749103342f
Steven Soloff <s_soloff@users.sourceforge.net>
2009-09-17 04:10:24 +0000

    Initial import.
    git-svn-id: https://svn.code.sf.net/p/gamegineer/code/dev/trunk@1796 897c5806-f142-0410-a293-e1bf5698dec8

---
+++ org.gamegineer.table.product.tests/src/org/gamegineer/table/internal/product/ApplicationTest.java
@@ -0,0 +1,98 @@
+/*
+ * ApplicationTest.java
+ * Copyright 2008-2009 Gamegineer.org
+ * All rights reserved.
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ * Created on Sep 16, 2009 at 11:43:29 PM.
+ */
+
+package org.gamegineer.table.internal.product;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+/**
+ * A fixture for testing the
+ * {@link org.gamegineer.table.internal.product.Application} class.
+ */
+public final class ApplicationTest
+{
+    // ======================================================================
+    // Fields
+    // ======================================================================
+
+    /** The application under test in the fixture. */
+    private Application application_;
+
+
+    // ======================================================================
+    // Constructors
+    // ======================================================================
+
+    /**
+     * Initializes a new instance of the {@code ApplicationTest} class.
+     */
+    public ApplicationTest()
+    {
+        super();
+    }
+
+
+    // ======================================================================
+    // Methods
+    // ======================================================================
+
+    /**
+     * Sets up the test fixture.
+     * 
+     * @throws java.lang.Exception
+     *         If an error occurs.
+     */
+    @Before
+    public void setUp()
+        throws Exception
+    {
+        application_ = new Application();
+    }
+
+    /**
+     * Tears down the test fixture.
+     * 
+     * @throws java.lang.Exception
+     *         If an error occurs.
+     */
+    @After
+    public void tearDown()
+        throws Exception
+    {
+        application_ = null;
+    }
+
+    /**
+     * Ensures the {@code start} method throws an exception when passed a
+     * {@code null} application context.
+     * 
+     * @throws java.lang.Exception
+     *         If an error occurs.
+     */
+    @Test( expected = NullPointerException.class )
+    public void testStart_Context_Null()
+        throws Exception
+    {
+        application_.start( null );
+    }
+}
---
+++ org.gamegineer.table.product.tests/src/org/gamegineer/table/product/AllTests.java
@@ -0,0 +1,59 @@
+/*
+ * AllTests.java
+ * Copyright 2008-2009 Gamegineer.org
+ * All rights reserved.
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ * Created on Sep 16, 2009 at 11:17:38 PM.
+ */
+
+package org.gamegineer.table.product;
+
+import junit.framework.Test;
+import org.gamegineer.table.internal.product.Activator;
+import org.gamegineer.test.core.BundleSuiteBuilder;
+
+/**
+ * Defines a test suite for running all tests in the fragment.
+ */
+public final class AllTests
+{
+    // ======================================================================
+    // Constructors
+    // ======================================================================
+
+    /**
+     * Initializes a new instance of the {@code AllTests} class.
+     */
+    public AllTests()
+    {
+        super();
+    }
+
+
+    // ======================================================================
+    // Methods
+    // ======================================================================
+
+    /**
+     * Creates a test suite consisting of all tests in the fragment.
+     * 
+     * @return A test suite consisting of all tests in the fragment.
+     */
+    public static Test suite()
+    {
+        return BundleSuiteBuilder.suite( Activator.getDefault().getBundleContext().getBundle(), "org.gamegineer.table.product.tests" ); //$NON-NLS-1$
+    }
+}
