bc26993cba3c669b2c2f2b9252b1ed9c8d9e0326
Steven Soloff <s_soloff@users.sourceforge.net>
2009-10-11 04:00:17 +0000

    Updated Javadocs.
    git-svn-id: https://svn.code.sf.net/p/gamegineer/code/dev/trunk@1817 897c5806-f142-0410-a293-e1bf5698dec8

--- org.gamegineer.table.core/src/org/gamegineer/table/core/ITable.java
+++ org.gamegineer.table.core/src/org/gamegineer/table/core/ITable.java
@@ -25,6 +25,8 @@ package org.gamegineer.table.core;
  * A virtual game table.
  * 
  * @noextend This interface is not intended to be extended by clients.
+ * 
+ * @noimplement This interface is not intended to be implemented by clients.
  */
 public interface ITable
 {
