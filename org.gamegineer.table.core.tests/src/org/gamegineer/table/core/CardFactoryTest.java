a7e139ade0261e0b0ef505824b62c3f35db31cc7
Steven Soloff <s_soloff@users.sourceforge.net>
2009-10-15 03:34:51 +0000

    Added a factory for creating cards.
    git-svn-id: https://svn.code.sf.net/p/gamegineer/code/dev/trunk@1820 897c5806-f142-0410-a293-e1bf5698dec8

---
+++ org.gamegineer.table.core.tests/src/org/gamegineer/table/core/CardFactoryTest.java
@@ -0,0 +1,78 @@
+/*
+ * CardFactoryTest.java
+ * Copyright 2008-2009 Gamegineer.org
+ * All rights reserved.
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ * Created on Oct 14, 2009 at 11:29:22 PM.
+ */
+
+package org.gamegineer.table.core;
+
+import static org.junit.Assert.assertNotNull;
+import org.junit.Test;
+
+/**
+ * A fixture for testing the {@link org.gamegineer.table.core.CardFactory}
+ * class.
+ */
+public final class CardFactoryTest
+{
+    // ======================================================================
+    // Constructors
+    // ======================================================================
+
+    /**
+     * Initializes a new instance of the {@code CardFactoryTest} class.
+     */
+    public CardFactoryTest()
+    {
+        super();
+    }
+
+
+    // ======================================================================
+    // Methods
+    // ======================================================================
+
+    /**
+     * Ensures the {@code createCard} method throws an exception when passed a
+     * {@code null} back design.
+     */
+    @Test( expected = NullPointerException.class )
+    public void testCreateCard_BackDesign_Null()
+    {
+        CardFactory.createCard( null, CardDesign.EMPTY );
+    }
+
+    /**
+     * Ensures the {@code createCard} method throws an exception when passed a
+     * {@code null} face design.
+     */
+    @Test( expected = NullPointerException.class )
+    public void testCreateCard_FaceDesign_Null()
+    {
+        CardFactory.createCard( CardDesign.EMPTY, null );
+    }
+
+    /**
+     * Ensures the {@code createCard} method does not return {@code null}.
+     */
+    @Test
+    public void testCreateCard_ReturnValue_NonNull()
+    {
+        assertNotNull( CardFactory.createCard( CardDesign.EMPTY, CardDesign.EMPTY ) );
+    }
+}
---
+++ org.gamegineer.table.core/src/org/gamegineer/table/core/CardFactory.java
@@ -0,0 +1,76 @@
+/*
+ * CardFactory.java
+ * Copyright 2008-2009 Gamegineer.org
+ * All rights reserved.
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ * Created on Oct 14, 2009 at 11:23:53 PM.
+ */
+
+package org.gamegineer.table.core;
+
+import static org.gamegineer.common.core.runtime.Assert.assertArgumentNotNull;
+import net.jcip.annotations.ThreadSafe;
+import org.gamegineer.table.internal.core.Card;
+
+/**
+ * A factory for creating cards.
+ */
+@ThreadSafe
+public final class CardFactory
+{
+    // ======================================================================
+    // Constructors
+    // ======================================================================
+
+    /**
+     * Initializes a new instance of the {@code CardFactory} class.
+     */
+    private CardFactory()
+    {
+        super();
+    }
+
+
+    // ======================================================================
+    // Methods
+    // ======================================================================
+
+    /**
+     * Creates a new card with the specified back and face designs.
+     * 
+     * @param backDesign
+     *        The design on the back of the card; must not be {@code null}.
+     * @param faceDesign
+     *        The design on the face of the card; must not be {@code null}.
+     * 
+     * @return A new card; never {@code null}.
+     * 
+     * @throws java.lang.NullPointerException
+     *         If {@code backDesign} or {@code faceDesign} is {@code null}.
+     */
+    /* @NonNull */
+    public static ICard createCard(
+        /* @NonNull */
+        final CardDesign backDesign,
+        /* @NonNull */
+        final CardDesign faceDesign )
+    {
+        assertArgumentNotNull( backDesign, "backDesign" ); //$NON-NLS-1$
+        assertArgumentNotNull( faceDesign, "faceDesign" ); //$NON-NLS-1$
+
+        return new Card( backDesign, faceDesign );
+    }
+}
