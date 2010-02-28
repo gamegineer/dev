2907b2958b928dc10a74a66dab43d7cd20e417b3
Steven Soloff <s_soloff@users.sourceforge.net>
2010-02-28 02:51:36 +0000

    Require CTRL key to be down to drag entire pile.
    git-svn-id: https://svn.code.sf.net/p/gamegineer/code/dev/trunk@1922 897c5806-f142-0410-a293-e1bf5698dec8

--- org.gamegineer.table.ui/src/org/gamegineer/table/internal/ui/view/TableView.java
+++ org.gamegineer.table.ui/src/org/gamegineer/table/internal/ui/view/TableView.java
@@ -30,6 +30,7 @@ import java.awt.Point;
 import java.awt.Rectangle;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.awt.event.InputEvent;
 import java.awt.event.KeyAdapter;
 import java.awt.event.KeyEvent;
 import java.awt.event.KeyListener;
@@ -808,9 +809,12 @@ final class TableView
             }
             else if( SwingUtilities.isLeftMouseButton( e ) )
             {
-                if( cardPile != null )
+                if( (e.getModifiersEx() & InputEvent.CTRL_DOWN_MASK) == InputEvent.CTRL_DOWN_MASK )
                 {
-                    setMouseInputHandler( DraggingCardPileMouseInputHandler.class, e );
+                    if( cardPile != null )
+                    {
+                        setMouseInputHandler( DraggingCardPileMouseInputHandler.class, e );
+                    }
                 }
             }
         }
