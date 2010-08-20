ca859ade1f3588e6b40b1ee62c1e9256ea2ec6be
Steven Soloff <s_soloff@users.sourceforge.net>
2010-08-20 02:03:37 +0000

    Initial import.
    git-svn-id: https://svn.code.sf.net/p/gamegineer/code/dev/trunk@2126 897c5806-f142-0410-a293-e1bf5698dec8

---
+++ org.gamegineer.table.core.persistence.serializable/src/org/gamegineer/table/core/package-info.java
@@ -0,0 +1,27 @@
+/*
+ * package-info.java
+ * Copyright 2008-2010 Gamegineer.org
+ * All rights reserved.
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ * Created on Aug 19, 2010 at 9:28:52 PM.
+ */
+
+/**
+ * Provides support for persisting classes in the
+ * {@code org.gamegineer.table.core} package using the Java object
+ * serialization framework.
+ */
+package org.gamegineer.table.core;
