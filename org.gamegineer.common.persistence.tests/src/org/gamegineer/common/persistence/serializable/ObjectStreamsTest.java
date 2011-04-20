f4d2b84e2932588b59814d039a2293d2c4bc0c25
Steven Soloff <s_soloff@users.sourceforge.net>
2011-04-20 23:45:52 +0000

    Added unit tests for the ObjectStreams class.
    git-svn-id: https://svn.code.sf.net/p/gamegineer/code/dev/trunk@2348 897c5806-f142-0410-a293-e1bf5698dec8

---
+++ org.gamegineer.common.persistence.tests/src/org/gamegineer/common/persistence/serializable/ObjectStreamsTest.java
@@ -0,0 +1,76 @@
+/*
+ * ObjectStreamsTest.java
+ * Copyright 2008-2011 Gamegineer.org
+ * All rights reserved.
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ * Created on Apr 20, 2011 at 7:42:19 PM.
+ */
+
+package org.gamegineer.common.persistence.serializable;
+
+import org.junit.Test;
+
+/**
+ * A fixture for testing the
+ * {@link org.gamegineer.common.persistence.serializable.ObjectStreams} class.
+ */
+public final class ObjectStreamsTest
+{
+    // ======================================================================
+    // Constructors
+    // ======================================================================
+
+    /**
+     * Initializes a new instance of the {@code ObjectStreamsTest} class.
+     */
+    public ObjectStreamsTest()
+    {
+        super();
+    }
+
+
+    // ======================================================================
+    // Methods
+    // ======================================================================
+
+    /**
+     * Ensures the {@code createPlatformObjectInputStream} method throws an
+     * exception when passed a {@code null} input stream.
+     * 
+     * @throws java.lang.Exception
+     *         If an error occurs.
+     */
+    @Test( expected = NullPointerException.class )
+    public void testCreatePlatformObjectInputStream_Stream_Null()
+        throws Exception
+    {
+        ObjectStreams.createPlatformObjectInputStream( null );
+    }
+
+    /**
+     * Ensures the {@code createPlatformObjectOutputStream} method throws an
+     * exception when passed a {@code null} output stream.
+     * 
+     * @throws java.lang.Exception
+     *         If an error occurs.
+     */
+    @Test( expected = NullPointerException.class )
+    public void testCreatePlatformObjectOutputStream_Stream_Null()
+        throws Exception
+    {
+        ObjectStreams.createPlatformObjectOutputStream( null );
+    }
+}
