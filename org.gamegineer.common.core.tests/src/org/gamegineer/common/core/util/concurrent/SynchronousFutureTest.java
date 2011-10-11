45e06f1c0bc821db7e02aba11f799ab648849a57
Steven Soloff <s_soloff@users.sourceforge.net>
2011-10-11 00:30:11 +0000

    Added the SynchronousFuture class.
    git-svn-id: https://svn.code.sf.net/p/gamegineer/code/dev/trunk@2539 897c5806-f142-0410-a293-e1bf5698dec8

---
+++ org.gamegineer.common.core.tests/src/org/gamegineer/common/core/util/concurrent/SynchronousFutureTest.java
@@ -0,0 +1,106 @@
+/*
+ * SynchronousFutureTest.java
+ * Copyright 2008-2011 Gamegineer.org
+ * All rights reserved.
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ * Created on Oct 10, 2011 at 8:11:39 PM.
+ */
+
+package org.gamegineer.common.core.util.concurrent;
+
+import static org.junit.Assert.assertSame;
+import static org.junit.Assert.fail;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.Future;
+import org.junit.Test;
+
+/**
+ * A fixture for testing the basic aspects of the
+ * {@link org.gamegineer.common.core.util.concurrent.SynchronousFuture} class.
+ */
+public final class SynchronousFutureTest
+{
+    // ======================================================================
+    // Constructors
+    // ======================================================================
+
+    /**
+     * Initializes a new instance of the {@code SynchronousFutureTest} class.
+     */
+    public SynchronousFutureTest()
+    {
+        super();
+    }
+
+
+    // ======================================================================
+    // Methods
+    // ======================================================================
+
+    /**
+     * Ensures the {@code SynchronousFuture(Throwable)} constructor throws an
+     * exception when passed a {@code null} exception.
+     */
+    @Test( expected = NullPointerException.class )
+    public void testConstructorFromException_Exception_Null()
+    {
+        new SynchronousFuture<Object>( (Throwable)null );
+    }
+
+    /**
+     * Ensures the {@code get()} method throws an exception when the future is
+     * constructed with an exception.
+     * 
+     * @throws java.lang.Exception
+     *         If an error occurs.
+     */
+    @Test
+    public void testGet_HasException()
+        throws Exception
+    {
+        final Throwable expectedValue = new Exception();
+        final Future<Object> future = new SynchronousFuture<Object>( expectedValue );
+
+        try
+        {
+            future.get();
+            fail( "expected ExecutionException" ); //$NON-NLS-1$
+        }
+        catch( final ExecutionException e )
+        {
+            assertSame( expectedValue, e.getCause() );
+        }
+    }
+
+    /**
+     * Ensures the {@code get()} method returns the expected value when the
+     * future is constructed with a result.
+     * 
+     * @throws java.lang.Exception
+     *         If an error occurs.
+     */
+    @Test
+    public void testGet_HasResult()
+        throws Exception
+    {
+        final Object expectedValue = new Object();
+        final Future<Object> future = new SynchronousFuture<Object>( expectedValue );
+
+        final Object actualValue = future.get();
+
+        assertSame( expectedValue, actualValue );
+    }
+}
---
+++ org.gamegineer.common.core/src/org/gamegineer/common/core/util/concurrent/SynchronousFuture.java
@@ -0,0 +1,178 @@
+/*
+ * SynchronousFuture.java
+ * Copyright 2008-2011 Gamegineer.org
+ * All rights reserved.
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ * Created on Oct 10, 2011 at 8:05:06 PM.
+ */
+
+package org.gamegineer.common.core.util.concurrent;
+
+import static org.gamegineer.common.core.runtime.Assert.assertArgumentNotNull;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.Future;
+import java.util.concurrent.TimeUnit;
+import net.jcip.annotations.Immutable;
+
+/**
+ * An implementation of {@link Future} used when the result of an asynchronous
+ * operation is computed synchronously.
+ * 
+ * @param <V>
+ *        The type of the operation result.
+ */
+@Immutable
+public final class SynchronousFuture<V>
+    implements Future<V>
+{
+    // ======================================================================
+    // Fields
+    // ======================================================================
+
+    /** The operation exception or {@code null} if no exception occurred. */
+    private final Throwable exception_;
+
+    /** The operation result. */
+    private final V result_;
+
+
+    // ======================================================================
+    // Constructors
+    // ======================================================================
+
+    /**
+     * Initializes a new instance of the {@code SynchronousFuture} class with a
+     * {@code null} result.
+     */
+    public SynchronousFuture()
+    {
+        this( null, null );
+    }
+
+    /**
+     * Initializes a new instance of the {@code SynchronousFuture} class with
+     * the specified result.
+     * 
+     * @param result
+     *        The operation result; may be {@code null}.
+     */
+    public SynchronousFuture(
+        /* @Nullable */
+        final V result )
+    {
+        this( result, null );
+    }
+
+    /**
+     * Initializes a new instance of the {@code SynchronousFuture} class with
+     * the specified exception.
+     * 
+     * @param exception
+     *        The operation exception; must not be {@code null}.
+     * 
+     * @throws java.lang.NullPointerException
+     *         If {@code exception} is {@code null}.
+     */
+    public SynchronousFuture(
+        /* @NonNull */
+        final Throwable exception )
+    {
+        this( null, exception );
+
+        assertArgumentNotNull( exception, "exception" ); //$NON-NLS-1$
+    }
+
+    /**
+     * Initializes a new instance of the {@code SynchronousFuture} class.
+     * 
+     * @param result
+     *        The operation result; may be {@code null}.
+     * @param exception
+     *        The operation exception; may be {@code null}.
+     */
+    private SynchronousFuture(
+        /* @Nullable */
+        final V result,
+        /* @Nullable */
+        final Throwable exception )
+    {
+        exception_ = exception;
+        result_ = result;
+    }
+
+
+    // ======================================================================
+    // Methods
+    // ======================================================================
+
+    /*
+     * @see java.util.concurrent.Future#cancel(boolean)
+     */
+    @Override
+    public boolean cancel(
+        @SuppressWarnings( "unused" )
+        final boolean mayInterruptIfRunning )
+    {
+        return false;
+    }
+
+    /*
+     * @see java.util.concurrent.Future#get()
+     */
+    @Override
+    public V get()
+        throws ExecutionException
+    {
+        if( exception_ != null )
+        {
+            throw new ExecutionException( exception_ );
+        }
+
+        return result_;
+    }
+
+    /*
+     * @see java.util.concurrent.Future#get(long, java.util.concurrent.TimeUnit)
+     */
+    @Override
+    public V get(
+        @SuppressWarnings( "unused" )
+        final long timeout,
+        @SuppressWarnings( "unused" )
+        final TimeUnit unit )
+        throws ExecutionException
+    {
+        return get();
+    }
+
+    /*
+     * @see java.util.concurrent.Future#isCancelled()
+     */
+    @Override
+    public boolean isCancelled()
+    {
+        return false;
+    }
+
+    /*
+     * @see java.util.concurrent.Future#isDone()
+     */
+    @Override
+    public boolean isDone()
+    {
+        return true;
+    }
+}
