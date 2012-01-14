b693f26a2cf8102ec87907d7fa4bca14e00bb7d2
Steven Soloff <s_soloff@users.sourceforge.net>
2012-01-14 02:47:17 +0000

    #61
    Initial import of table help set provider and files.
    
    git-svn-id: https://svn.code.sf.net/p/gamegineer/code/dev/trunk@2660 897c5806-f142-0410-a293-e1bf5698dec8

---
+++ org.gamegineer.table.help/help/table.hs
@@ -0,0 +1,29 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<!DOCTYPE helpset PUBLIC "-//Sun Microsystems Inc.//DTD JavaHelp HelpSet Version 2.0//EN" "http://java.sun.com/products/javahelp/helpset_2_0.dtd">
+<helpset version="2.0">
+	<title>Gamegineer</title>
+
+	<maps>
+		<homeID>main</homeID>
+		<mapref location="map.jhm" />
+	</maps>
+
+	<view>
+		<name>Contents</name>
+		<label>Contents</label>
+		<type>javax.help.TOCView</type>
+		<data>toc.xml</data>
+	</view>
+	<view>
+		<name>Index</name>
+		<label>Index</label>
+		<type>javax.help.IndexView</type>
+		<data>index.xml</data>
+	</view>
+	<view>
+		<name>Search</name>
+		<label>Search</label>
+		<type>javax.help.SearchView</type>
+		<data engine="com.sun.java.help.search.DefaultSearchEngine">JavaHelpSearch</data>
+	</view>
+</helpset>
---
+++ org.gamegineer.table.help/src/org/gamegineer/table/internal/help/HelpSetProvider.java
@@ -0,0 +1,91 @@
+/*
+ * HelpSetProvider.java
+ * Copyright 2008-2012 Gamegineer.org
+ * All rights reserved.
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ * Created on Jan 13, 2012 at 8:46:02 PM.
+ */
+
+package org.gamegineer.table.internal.help;
+
+import java.net.URL;
+import javax.help.HelpSet;
+import javax.help.HelpSetException;
+import net.jcip.annotations.GuardedBy;
+import net.jcip.annotations.ThreadSafe;
+import org.gamegineer.common.ui.help.IHelpSetProvider;
+
+/**
+ * Provides the table help set.
+ */
+@ThreadSafe
+public final class HelpSetProvider
+    implements IHelpSetProvider
+{
+    // ======================================================================
+    // Fields
+    // ======================================================================
+
+    /** The help set. */
+    @GuardedBy( "lock_" )
+    private HelpSet helpSet_;
+
+    /** The instance lock. */
+    private final Object lock_;
+
+
+    // ======================================================================
+    // Constructors
+    // ======================================================================
+
+    /**
+     * Initializes a new instance of the {@code HelpSetProvider} class.
+     */
+    public HelpSetProvider()
+    {
+        helpSet_ = null;
+        lock_ = new Object();
+    }
+
+
+    // ======================================================================
+    // Methods
+    // ======================================================================
+
+    /*
+     * @see org.gamegineer.common.ui.help.IHelpSetProvider#getHelpSet()
+     */
+    @Override
+    public HelpSet getHelpSet()
+        throws HelpSetException
+    {
+        synchronized( lock_ )
+        {
+            if( helpSet_ == null )
+            {
+                final URL helpSetUrl = Activator.getDefault().getBundleContext().getBundle().getEntry( "/help/table.hs" ); //$NON-NLS-1$
+                if( helpSetUrl == null )
+                {
+                    throw new HelpSetException( NonNlsMessages.HelpSetProvider_getHelpSet_helpSetNotAvailable );
+                }
+
+                helpSet_ = new HelpSet( null, helpSetUrl );
+            }
+
+            return helpSet_;
+        }
+    }
+}
