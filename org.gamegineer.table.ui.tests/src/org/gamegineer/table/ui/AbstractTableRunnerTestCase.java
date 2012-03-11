8e49cf10d81761e79bd54c6926a8bdf96979eaf0
Steven Soloff <s_soloff@users.sourceforge.net>
2012-03-11 03:31:40 +0000

    Increase timeout for executor to shut down during test.
    git-svn-id: https://svn.code.sf.net/p/gamegineer/code/dev/trunk@2743 897c5806-f142-0410-a293-e1bf5698dec8

--- org.gamegineer.table.ui.tests/src/org/gamegineer/table/ui/AbstractTableRunnerTestCase.java
+++ org.gamegineer.table.ui.tests/src/org/gamegineer/table/ui/AbstractTableRunnerTestCase.java
@@ -147,7 +147,7 @@ public abstract class AbstractTableRunnerTestCase
         finally
         {
             executor.shutdownNow();
-            assertTrue( "executor did not shut down cleanly", executor.awaitTermination( 5L, TimeUnit.SECONDS ) ); //$NON-NLS-1$
+            assertTrue( "executor did not shut down cleanly", executor.awaitTermination( 10L, TimeUnit.SECONDS ) ); //$NON-NLS-1$
         }
     }
 }
