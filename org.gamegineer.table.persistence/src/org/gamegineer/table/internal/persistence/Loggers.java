580f56ac1b82eb6230ecb6075f4b17454da0f69b
Steven Soloff <s_soloff@users.sourceforge.net>
2012-03-16 00:25:04 +0000

    Initial import of the org.gamegineer.table.persistence bundles.
    git-svn-id: https://svn.code.sf.net/p/gamegineer/code/dev/trunk@2747 897c5806-f142-0410-a293-e1bf5698dec8

---
+++ org.gamegineer.table.persistence/src/org/gamegineer/table/internal/persistence/Loggers.java
@@ -0,0 +1,60 @@
+/*
+ * Loggers.java
+ * Copyright 2008-2012 Gamegineer.org
+ * All rights reserved.
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ * Created on Mar 13, 2012 at 8:57:44 PM.
+ */
+
+package org.gamegineer.table.internal.persistence;
+
+import java.util.logging.Logger;
+import net.jcip.annotations.ThreadSafe;
+
+/**
+ * Manages the loggers used by the bundle.
+ */
+@ThreadSafe
+public final class Loggers
+    extends org.gamegineer.common.core.runtime.Loggers
+{
+    // ======================================================================
+    // Constructors
+    // ======================================================================
+
+    /**
+     * Initializes a new instance of the {@code Loggers} class.
+     */
+    private Loggers()
+    {
+    }
+
+
+    // ======================================================================
+    // Methods
+    // ======================================================================
+
+    /**
+     * Gets the default logger for the bundle.
+     * 
+     * @return The default logger for the bundle; never {@code null}.
+     */
+    /* @NonNull */
+    public static Logger getDefaultLogger()
+    {
+        return getLogger( Activator.getDefault().getBundleContext().getBundle(), null );
+    }
+}
