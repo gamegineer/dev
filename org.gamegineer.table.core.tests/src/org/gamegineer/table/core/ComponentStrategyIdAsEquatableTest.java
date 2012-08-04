b9a095ddd293e946fd9d056f21169bf3d3fc1926
Steven Soloff <s_soloff@users.sourceforge.net>
2012-08-04 01:07:34 +0000

    Added the ComponentStrategyId class.
    git-svn-id: https://svn.code.sf.net/p/gamegineer/code/dev/trunk@2872 897c5806-f142-0410-a293-e1bf5698dec8

---
+++ org.gamegineer.table.core.tests/src/org/gamegineer/table/core/ComponentStrategyIdAsEquatableTest.java
@@ -0,0 +1,72 @@
+/*
+ * ComponentStrategyIdAsEquatableTest.java
+ * Copyright 2008-2012 Gamegineer.org
+ * All rights reserved.
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ * Created on Aug 3, 2012 at 9:03:26 PM.
+ */
+
+package org.gamegineer.table.core;
+
+import java.util.ArrayList;
+import java.util.Collection;
+import org.gamegineer.test.core.AbstractEquatableTestCase;
+
+/**
+ * A fixture for testing the
+ * {@link org.gamegineer.table.core.ComponentStrategyId} class to ensure it does
+ * not violate the contract of the equatable interface.
+ */
+public final class ComponentStrategyIdAsEquatableTest
+    extends AbstractEquatableTestCase<ComponentStrategyId>
+{
+    // ======================================================================
+    // Constructors
+    // ======================================================================
+
+    /**
+     * Initializes a new instance of the
+     * {@code ComponentStrategyIdAsEquatableTest} class.
+     */
+    public ComponentStrategyIdAsEquatableTest()
+    {
+    }
+
+
+    // ======================================================================
+    // Methods
+    // ======================================================================
+
+    /*
+     * @see org.gamegineer.test.core.AbstractEquatableTestCase#createReferenceInstance()
+     */
+    @Override
+    protected ComponentStrategyId createReferenceInstance()
+    {
+        return ComponentStrategyId.fromString( "id" ); //$NON-NLS-1$
+    }
+
+    /*
+     * @see org.gamegineer.test.core.AbstractEquatableTestCase#createUnequalInstances()
+     */
+    @Override
+    protected Collection<ComponentStrategyId> createUnequalInstances()
+    {
+        final Collection<ComponentStrategyId> others = new ArrayList<ComponentStrategyId>();
+        others.add( ComponentStrategyId.fromString( "otherId" ) ); //$NON-NLS-1$
+        return others;
+    }
+}
---
+++ org.gamegineer.table.core.tests/src/org/gamegineer/table/core/ComponentStrategyIdTest.java
@@ -0,0 +1,85 @@
+/*
+ * ComponentStrategyIdTest.java
+ * Copyright 2008-2012 Gamegineer.org
+ * All rights reserved.
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ * Created on Aug 3, 2012 at 9:03:14 PM.
+ */
+
+package org.gamegineer.table.core;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
+import org.junit.Test;
+
+/**
+ * A fixture for testing the
+ * {@link org.gamegineer.table.core.ComponentStrategyId} class.
+ */
+public final class ComponentStrategyIdTest
+{
+    // ======================================================================
+    // Constructors
+    // ======================================================================
+
+    /**
+     * Initializes a new instance of the {@code ComponentStrategyIdTest} class.
+     */
+    public ComponentStrategyIdTest()
+    {
+    }
+
+
+    // ======================================================================
+    // Methods
+    // ======================================================================
+
+    /**
+     * Ensures the {@code fromString} method throws an exception when passed a
+     * {@code null} underlying identifier.
+     */
+    @Test( expected = NullPointerException.class )
+    public void testFromString_Id_Null()
+    {
+        ComponentStrategyId.fromString( null );
+    }
+
+    /**
+     * Ensures the {@code toString} method does not return {@code null}.
+     */
+    @Test
+    public void testToString_ReturnValue_NonNull()
+    {
+        final ComponentStrategyId id = ComponentStrategyId.fromString( "id" ); //$NON-NLS-1$
+
+        assertNotNull( id.toString() );
+    }
+
+    /**
+     * Ensures the {@code toString} method returns the same underlying
+     * identifier that was used to construct the component strategy identifier.
+     */
+    @Test
+    public void testToString_ReturnValue_SameId()
+    {
+        final String expectedId = "id"; //$NON-NLS-1$
+        final ComponentStrategyId id = ComponentStrategyId.fromString( expectedId );
+
+        final String actualId = id.toString();
+
+        assertEquals( expectedId, actualId );
+    }
+}
