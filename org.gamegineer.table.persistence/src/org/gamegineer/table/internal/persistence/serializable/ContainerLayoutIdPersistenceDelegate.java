3cddbd61cdb2e0c3d11a557148681f2f6189bae7
Steven Soloff <s_soloff@users.sourceforge.net>
2012-08-12 00:32:21 +0000

    Added a persistence delegate for the ContainerLayoutId class.
    git-svn-id: https://svn.code.sf.net/p/gamegineer/code/dev/trunk@2888 897c5806-f142-0410-a293-e1bf5698dec8

---
+++ org.gamegineer.table.persistence/src/org/gamegineer/table/internal/persistence/serializable/ContainerLayoutIdPersistenceDelegate.java
@@ -0,0 +1,68 @@
+/*
+ * ContainerLayoutIdPersistenceDelegate.java
+ * Copyright 2008-2012 Gamegineer.org
+ * All rights reserved.
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ * Created on Aug 11, 2012 at 8:24:04 PM.
+ */
+
+package org.gamegineer.table.internal.persistence.serializable;
+
+import java.io.IOException;
+import net.jcip.annotations.Immutable;
+import org.gamegineer.common.persistence.serializable.AbstractPersistenceDelegate;
+import org.gamegineer.table.core.ContainerLayoutId;
+
+/**
+ * A persistence delegate for the {@code ContainerLayoutId} class.
+ */
+@Immutable
+public final class ContainerLayoutIdPersistenceDelegate
+    extends AbstractPersistenceDelegate
+{
+    // ======================================================================
+    // Constructors
+    // ======================================================================
+
+    /**
+     * Initializes a new instance of the
+     * {@code ContainerLayoutIdPersistenceDelegate} class.
+     */
+    public ContainerLayoutIdPersistenceDelegate()
+    {
+    }
+
+
+    // ======================================================================
+    // Methods
+    // ======================================================================
+
+    /*
+     * @see org.gamegineer.common.persistence.serializable.AbstractPersistenceDelegate#replaceObject(java.lang.Object)
+     */
+    @Override
+    public Object replaceObject(
+        final Object obj )
+        throws IOException
+    {
+        if( obj instanceof ContainerLayoutId )
+        {
+            return new ContainerLayoutIdProxy( (ContainerLayoutId)obj );
+        }
+
+        return super.replaceObject( obj );
+    }
+}
---
+++ org.gamegineer.table.persistence/src/org/gamegineer/table/internal/persistence/serializable/ContainerLayoutIdProxy.java
@@ -0,0 +1,100 @@
+/*
+ * ContainerLayoutIdProxy.java
+ * Copyright 2008-2012 Gamegineer.org
+ * All rights reserved.
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ * Created on Aug 11, 2012 at 8:24:17 PM.
+ */
+
+package org.gamegineer.table.internal.persistence.serializable;
+
+import static org.gamegineer.common.core.runtime.Assert.assertArgumentNotNull;
+import java.io.Serializable;
+import net.jcip.annotations.NotThreadSafe;
+import org.gamegineer.table.core.ContainerLayoutId;
+
+/**
+ * A serializable proxy for the {@code ContainerLayoutId} class.
+ */
+@NotThreadSafe
+public final class ContainerLayoutIdProxy
+    implements Serializable
+{
+    // ======================================================================
+    // Fields
+    // ======================================================================
+
+    /** Serializable class version number. */
+    private static final long serialVersionUID = 3886747828720997230L;
+
+    /**
+     * The container layout identifier.
+     * 
+     * @serial
+     */
+    private String id_;
+
+
+    // ======================================================================
+    // Constructors
+    // ======================================================================
+
+    /**
+     * Initializes a new instance of the {@code ContainerLayoutIdProxy} class.
+     */
+    @SuppressWarnings( "unused" )
+    private ContainerLayoutIdProxy()
+    {
+        id_ = null;
+    }
+
+    /**
+     * Initializes a new instance of the {@code ContainerLayoutIdProxy} class
+     * from the specified {@code ContainerLayoutId} instance.
+     * 
+     * @param containerLayoutId
+     *        The {@code ContainerLayoutId} instance; must not be {@code null}.
+     * 
+     * @throws java.lang.NullPointerException
+     *         If {@code containerLayoutId} is {@code null}.
+     */
+    public ContainerLayoutIdProxy(
+        /* @NonNull */
+        final ContainerLayoutId containerLayoutId )
+    {
+        assertArgumentNotNull( containerLayoutId, "containerLayoutId" ); //$NON-NLS-1$
+
+        id_ = containerLayoutId.toString();
+    }
+
+
+    // ======================================================================
+    // Methods
+    // ======================================================================
+
+    /**
+     * Gets a replacement object for this instance after it has been
+     * deserialized.
+     * 
+     * @return A replacement object for this instance after it has been
+     *         deserialized; never {@code null}.
+     */
+    /* @NonNull */
+    private Object readResolve()
+    {
+        return ContainerLayoutId.fromString( id_ );
+    }
+}
