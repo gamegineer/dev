c0ef52fe6d2d511e94cb50b5e287fcc0c2e7a72a
Steven Soloff <s_soloff@users.sourceforge.net>
2012-09-30 01:55:18 +0000

    Added tests for the CardStrategyUI, CardPileStrategyUI, and TabletopStrategyUI classes.
    git-svn-id: https://svn.code.sf.net/p/gamegineer/code/dev/trunk@2950 897c5806-f142-0410-a293-e1bf5698dec8

---
+++ org.gamegineer.table.ui.tests/src/org/gamegineer/table/internal/ui/strategies/TabletopStrategyUIAsContainerStrategyUITest.java
@@ -0,0 +1,60 @@
+/*
+ * TabletopStrategyUIAsContainerStrategyUITest.java
+ * Copyright 2008-2012 Gamegineer.org
+ * All rights reserved.
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ * Created on Sep 29, 2012 at 9:49:14 PM.
+ */
+
+package org.gamegineer.table.internal.ui.strategies;
+
+import org.gamegineer.table.ui.AbstractContainerStrategyUITestCase;
+
+/**
+ * A fixture for testing the
+ * {@link org.gamegineer.table.internal.ui.strategies.TabletopStrategyUI} class
+ * to ensure it does not violate the contract of the
+ * {@link org.gamegineer.table.ui.IContainerStrategyUI} interface.
+ */
+public final class TabletopStrategyUIAsContainerStrategyUITest
+    extends AbstractContainerStrategyUITestCase<TabletopStrategyUI>
+{
+    // ======================================================================
+    // Constructors
+    // ======================================================================
+
+    /**
+     * Initializes a new instance of the
+     * {@code TabletopStrategyUIAsContainerStrategyUITest} class.
+     */
+    public TabletopStrategyUIAsContainerStrategyUITest()
+    {
+    }
+
+
+    // ======================================================================
+    // Methods
+    // ======================================================================
+
+    /*
+     * @see org.gamegineer.table.ui.AbstractComponentStrategyUITestCase#createComponentStrategyUI()
+     */
+    @Override
+    protected TabletopStrategyUI createComponentStrategyUI()
+    {
+        return new TabletopStrategyUI();
+    }
+}
