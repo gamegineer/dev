7a7b424d0d5e8ed9c55193113e7cc9f959517421
Steven Soloff <s_soloff@users.sourceforge.net>
2013-01-18 04:45:11 +0000

    Added the AbstractComparableTestCase class.
    git-svn-id: https://svn.code.sf.net/p/gamegineer/code/dev/trunk@3030 897c5806-f142-0410-a293-e1bf5698dec8

---
+++ org.gamegineer.test.core/src/org/gamegineer/test/core/AbstractComparableTestCase.java
@@ -0,0 +1,156 @@
+/*
+ * AbstractComparableTestCase.java
+ * Copyright 2008-2013 Gamegineer.org
+ * All rights reserved.
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ * Created on Jan 17, 2013 at 10:59:15 PM.
+ */
+
+package org.gamegineer.test.core;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
+import java.util.Collection;
+import org.junit.Test;
+
+/**
+ * A fixture for testing the basic aspects of classes that implement the
+ * {@link java.lang.Comparable} interface.
+ * 
+ * @param <T>
+ *        The type of the comparable class.
+ */
+public abstract class AbstractComparableTestCase<T extends Comparable<T>>
+    extends AbstractEquatableTestCase<T>
+{
+    // ======================================================================
+    // Constructors
+    // ======================================================================
+
+    /**
+     * Initializes a new instance of the {@code AbstractComparableTestCase}
+     * class.
+     */
+    protected AbstractComparableTestCase()
+    {
+    }
+
+
+    // ======================================================================
+    // Methods
+    // ======================================================================
+
+    /**
+     * Creates a collection of instances that are greater than the reference
+     * instance.
+     * 
+     * <p>
+     * Implementors should return one instance per field that is used to
+     * determine comparability. The returned collection must not be empty and
+     * must not contain a {@code null} entry.
+     * </p>
+     * 
+     * @return A collection of instances that are greater than the reference
+     *         instance; never {@code null}.
+     * 
+     * @throws java.lang.Exception
+     *         If an error occurs.
+     */
+    /* @NonNull */
+    protected abstract Collection<T> createGreaterThanInstances()
+        throws Exception;
+
+    /**
+     * Creates a collection of instances that are less than the reference
+     * instance.
+     * 
+     * <p>
+     * Implementors should return one instance per field that is used to
+     * determine comparability. The returned collection must not be empty and
+     * must not contain a {@code null} entry.
+     * </p>
+     * 
+     * @return A collection of instances that are less than the reference
+     *         instance; never {@code null}.
+     * 
+     * @throws java.lang.Exception
+     *         If an error occurs.
+     */
+    /* @NonNull */
+    protected abstract Collection<T> createLessThanInstances()
+        throws Exception;
+
+    /**
+     * Ensures the {@link Comparable#compareTo} method correctly indicates
+     * instances that are equal to the reference instance are equal to the
+     * reference instance.
+     * 
+     * @throws java.lang.Exception
+     *         If an error occurs.
+     */
+    @Test
+    public void testCompareTo_Equal()
+        throws Exception
+    {
+        final T reference1 = createReferenceInstance();
+        final T reference2 = createReferenceInstance();
+
+        assertEquals( 0, reference1.compareTo( reference2 ) );
+        assertEquals( 0, reference2.compareTo( reference1 ) );
+    }
+
+    /**
+     * Ensures the {@link Comparable#compareTo} method correctly indicates
+     * instances that are greater than the reference instance are greater than
+     * the reference instance.
+     * 
+     * @throws java.lang.Exception
+     *         If an error occurs.
+     */
+    @Test
+    public void testCompareTo_GreaterThan()
+        throws Exception
+    {
+        final T reference = createReferenceInstance();
+
+        for( final T other : createGreaterThanInstances() )
+        {
+            assertTrue( String.format( "expected <%s> to be less than <%s>", reference, other ), reference.compareTo( other ) < 0 ); //$NON-NLS-1$
+            assertTrue( String.format( "expected <%s> to be greater than <%s>", other, reference ), other.compareTo( reference ) > 0 ); //$NON-NLS-1$
+        }
+    }
+
+    /**
+     * Ensures the {@link Comparable#compareTo} method correctly indicates
+     * instances that are less than the reference instance are less than the
+     * reference instance.
+     * 
+     * @throws java.lang.Exception
+     *         If an error occurs.
+     */
+    @Test
+    public void testCompareTo_LessThan()
+        throws Exception
+    {
+        final T reference = createReferenceInstance();
+
+        for( final T other : createLessThanInstances() )
+        {
+            assertTrue( String.format( "expected <%s> to be greater than <%s>", reference, other ), reference.compareTo( other ) > 0 ); //$NON-NLS-1$
+            assertTrue( String.format( "expected <%s> to be less than <%s>", other, reference ), other.compareTo( reference ) < 0 ); //$NON-NLS-1$
+        }
+    }
+}
