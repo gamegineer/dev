c9285ba42a6499e1227295001d036004465b0c9a
Steven Soloff <s_soloff@users.sourceforge.net>
2013-08-23 02:04:53 +0000

    Added development environment template script.
    git-svn-id: https://svn.code.sf.net/p/gamegineer/code/dev/trunk@3184 897c5806-f142-0410-a293-e1bf5698dec8

---
+++ settings/shell/dev-environment.bat
@@ -0,0 +1,54 @@
+@ECHO OFF
+
+REM =========================================================================
+REM
+REM dev-environment.bat
+REM Copyright 2008-2013 Gamegineer.org
+REM All rights reserved.
+REM
+REM This is a sample development environment setup script required for a
+REM Windows-based Gamegineer development workstation.  You should download
+REM this file to your workstation and customize the variable definitions
+REM between the BEGIN-CUSTOM-VARIABLES and END-CUSTOM-VARIABLES markers.
+REM The customized file should then be archived on your workstation and be
+REM part of your development environment startup process.
+REM
+REM =========================================================================
+
+REM -------------------------------------------------------------------------
+REM
+REM BEGIN-CUSTOM-VARIABLES
+REM
+
+REM
+REM Home directories of all build tools.
+REM
+SET ANT_HOME=C:\Program Files\apache-ant-1.9.2
+SET ECLIPSE_HOME=C:\Program Files\eclipse-3.7.2
+SET JAVA_HOME=C:\Program Files\Java\jdk1.7.0_25
+SET MAVEN_HOME=C:\Program Files\apache-maven-3.0.5
+SET SVN_HOME=C:\Program Files\Subversion
+
+REM
+REM Build tool options.
+REM
+SET ECLIPSE_OPTS=-Xmx512M
+
+REM
+REM END-CUSTOM-VARIABLES
+REM
+REM -------------------------------------------------------------------------
+
+REM
+REM Update path to include all build-related binaries.
+REM
+SET PATH=%JAVA_HOME%\bin;%ANT_HOME%\bin;%MAVEN_HOME%\bin;%ECLIPSE_HOME%;%SVN_HOME%\bin;%PATH%
+
+REM
+REM Create useful macros.
+REM
+DOSKEY eclipse-dev=eclipse -vm "%JAVA_HOME%\jre\bin\javaw.exe" -vmargs %ECLIPSE_OPTS%
+
+ECHO Gamegineer Development Environment
+ECHO.
+ECHO Type 'eclipse-dev' to start the Eclipse IDE.
