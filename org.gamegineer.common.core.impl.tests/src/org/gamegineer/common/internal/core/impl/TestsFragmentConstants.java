065c71195e4d070eaae6f63d9e3a151276a0ddad
Steven Soloff <s_soloff@users.sourceforge.net>
2013-08-31 00:59:39 +0000

    Added new org.gamegineer.common projects to build.
    git-svn-id: https://svn.code.sf.net/p/gamegineer/code/dev/trunk@3198 897c5806-f142-0410-a293-e1bf5698dec8

---
+++ org.gamegineer.common.core.impl.tests/src/org/gamegineer/common/internal/core/impl/TestsFragmentConstants.java
@@ -0,0 +1,50 @@
+/*
+ * TestsFragmentConstants.java
+ * Copyright 2008-2013 Gamegineer.org
+ * All rights reserved.
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ * Created on Aug 30, 2013 at 8:22:29 PM.
+ */
+
+package org.gamegineer.common.internal.core.impl;
+
+import net.jcip.annotations.ThreadSafe;
+
+/**
+ * Defines useful constants for use by the fragment.
+ */
+@ThreadSafe
+public final class TestsFragmentConstants
+{
+    // ======================================================================
+    // Fields
+    // ======================================================================
+
+    /** The symbolic name of the fragment. */
+    public static final String SYMBOLIC_NAME = "org.gamegineer.common.core.impl.tests"; //$NON-NLS-1$
+
+
+    // ======================================================================
+    // Constructors
+    // ======================================================================
+
+    /**
+     * Initializes a new instance of the {@code TestsFragmentConstants} class.
+     */
+    private TestsFragmentConstants()
+    {
+    }
+}
