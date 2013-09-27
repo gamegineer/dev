c53bda7b7cd7e737f76f2e7116d10f214143dd9b
Steven Soloff <s_soloff@users.sourceforge.net>
2013-09-27 02:47:06 +0000

    Added an activator in preparation for the addition of a unit test fragment.
    git-svn-id: https://svn.code.sf.net/p/gamegineer/code/dev/trunk@3250 897c5806-f142-0410-a293-e1bf5698dec8

---
+++ main/common/org.gamegineer.common.ui.test/src/org/gamegineer/common/internal/ui/test/BundleConstants.java
@@ -0,0 +1,50 @@
+/*
+ * BundleConstants.java
+ * Copyright 2008-2013 Gamegineer contributors and others.
+ * All rights reserved.
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ * Created on Sep 26, 2013 at 10:42:24 PM.
+ */
+
+package org.gamegineer.common.internal.ui.test;
+
+import net.jcip.annotations.ThreadSafe;
+
+/**
+ * Defines useful constants for use by the bundle.
+ */
+@ThreadSafe
+public final class BundleConstants
+{
+    // ======================================================================
+    // Fields
+    // ======================================================================
+
+    /** The symbolic name of the bundle. */
+    public static final String SYMBOLIC_NAME = "org.gamegineer.common.ui.test"; //$NON-NLS-1$
+
+
+    // ======================================================================
+    // Constructors
+    // ======================================================================
+
+    /**
+     * Initializes a new instance of the {@code BundleConstants} class.
+     */
+    private BundleConstants()
+    {
+    }
+}
