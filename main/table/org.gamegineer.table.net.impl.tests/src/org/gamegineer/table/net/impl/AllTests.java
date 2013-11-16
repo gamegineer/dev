8653d7c0ae41f2e97d61806d68c9b2245ab3fdca
Steven Soloff <s_soloff@users.sourceforge.net>
2013-11-16 01:40:02 +0000

    Added new org.gamegineer.table projects to build.
    git-svn-id: https://svn.code.sf.net/p/gamegineer/code/dev/trunk@3286 897c5806-f142-0410-a293-e1bf5698dec8

---
+++ main/table/org.gamegineer.table.net.impl.tests/src/org/gamegineer/table/net/impl/AllTests.java
@@ -0,0 +1,61 @@
+/*
+ * AllTests.java
+ * Copyright 2008-2013 Gamegineer contributors and others.
+ * All rights reserved.
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ * Created on Nov 15, 2013 at 8:27:59 PM.
+ */
+
+package org.gamegineer.table.net.impl;
+
+import junit.framework.Test;
+import org.gamegineer.table.internal.net.impl.Activator;
+import org.gamegineer.table.internal.net.impl.TestsFragmentConstants;
+import org.gamegineer.test.core.BundleSuiteBuilder;
+
+/**
+ * Defines a test suite for running all tests in the fragment.
+ */
+public final class AllTests
+{
+    // ======================================================================
+    // Constructors
+    // ======================================================================
+
+    /**
+     * Initializes a new instance of the {@code AllTests} class.
+     */
+    public AllTests()
+    {
+    }
+
+
+    // ======================================================================
+    // Methods
+    // ======================================================================
+
+    /**
+     * Creates a test suite consisting of all tests in the fragment.
+     * 
+     * @return A test suite consisting of all tests in the fragment; never
+     *         {@code null}.
+     */
+    /* @NonNull */
+    public static Test suite()
+    {
+        return BundleSuiteBuilder.suite( Activator.getDefault().getBundleContext().getBundle(), TestsFragmentConstants.SYMBOLIC_NAME );
+    }
+}
