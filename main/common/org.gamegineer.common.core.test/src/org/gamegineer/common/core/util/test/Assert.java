1f48f1c96be5e1e002e122444623672d85a37300
Steven Soloff <s_soloff@yahoo.com>
2015-09-11 16:05:03 -0400

    Remove unnecessary cast.
    Warning identified after upgrading JDT to Mars.

--- main/common/org.gamegineer.common.core.test/src/org/gamegineer/common/core/util/test/Assert.java
+++ main/common/org.gamegineer.common.core.test/src/org/gamegineer/common/core/util/test/Assert.java
@@ -1,6 +1,6 @@
 /*
  * Assert.java
- * Copyright 2008-2014 Gamegineer contributors and others.
+ * Copyright 2008-2015 Gamegineer contributors and others.
  * All rights reserved.
  *
  * This program is free software: you can redistribute it and/or modify
@@ -167,7 +167,7 @@ public final class Assert
         }
 
         @SuppressWarnings( "unchecked" )
-        final IEqualityComparator<Object> equalityComparator = (IEqualityComparator<Object>)adapterManager.getAdapter( obj, IEqualityComparator.class );
+        final IEqualityComparator<Object> equalityComparator = adapterManager.getAdapter( obj, IEqualityComparator.class );
         return equalityComparator;
     }
 }
