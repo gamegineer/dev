gamegineer/dev

commit 66f182ee79af70c7ddd55e8d2fec984644626503

Fix errors/warnings due to Eclipse Mars upgrade.

main/table/org.gamegineer.table.core/src/org/gamegineer/table/core/ComponentPath.java
@@ -1,6 +1,6 @@
 /*
  * ComponentPath.java
- * Copyright 2008-2014 Gamegineer contributors and others.
+ * Copyright 2008-2015 Gamegineer contributors and others.
  * All rights reserved.
  *
  * This program is free software: you can redistribute it and/or modify
@@ -98,8 +98,11 @@ public final class ComponentPath
      */
     @Override
     public int compareTo(
+        @Nullable
         final ComponentPath other )
     {
+        assert other != null;
+
         final int parentPathCompareResult = ComparableUtils.compareTo( parentPath_, other.parentPath_ );
         if( parentPathCompareResult != 0 )
         {

main/table/org.gamegineer.table.ui.impl/src/org/gamegineer/table/internal/ui/impl/util/swing/SortedListModel.java
@@ -1,6 +1,6 @@
 /*
  * SortedListModel.java
- * Copyright 2008-2014 Gamegineer contributors and others.
+ * Copyright 2008-2015 Gamegineer contributors and others.
  * All rights reserved.
  *
  * This program is free software: you can redistribute it and/or modify
@@ -120,9 +120,14 @@ public final class SortedListModel<E>
             @Override
             @SuppressWarnings( "unchecked" )
             public int compare(
+                @Nullable
                 final E o1,
+                @Nullable
                 final E o2 )
             {
+                assert o1 != null;
+                assert o2 != null;
+
                 return ((Comparable<E>)o1).compareTo( o2 );
             }
         };
@@ -388,8 +393,11 @@ public final class SortedListModel<E>
             "synthetic-access", "unchecked"
         } )
         public int compareTo(
+            @Nullable
             final Entry other )
         {
+            assert other != null;
+
             final Object element = unsortedListModel_.getElementAt( index_ );
             final Object otherElement = unsortedListModel_.getElementAt( other.getIndex() );
             return ((Comparator<Object>)comparator_).compare( element, otherElement );

main/common/org.gamegineer.common.core.impl/src/org/gamegineer/common/internal/core/impl/util/concurrent/ExecutorService.java
@@ -1,6 +1,6 @@
 /*
  * ExecutorService.java
- * Copyright 2008-2014 Gamegineer contributors and others.
+ * Copyright 2008-2015 Gamegineer contributors and others.
  * All rights reserved.
  *
  * This program is free software: you can redistribute it and/or modify
@@ -309,6 +309,7 @@ public final class ExecutorService
     @Nullable
     @Override
     public <T> Future<T> submit(
+        @Nullable
         final Callable<T> task )
     {
         assert task != null;
@@ -336,7 +337,9 @@ public final class ExecutorService
     @Nullable
     @Override
     public <T> Future<T> submit(
+        @Nullable
         final Runnable task,
+        @Nullable
         final T result )
     {
         assert task != null;

main/table/org.gamegineer.table.core.impl/src/org/gamegineer/table/internal/core/impl/Table.java
@@ -1,6 +1,6 @@
 /*
  * Table.java
- * Copyright 2008-2014 Gamegineer contributors and others.
+ * Copyright 2008-2015 Gamegineer contributors and others.
  * All rights reserved.
  *
  * This program is free software: you can redistribute it and/or modify
@@ -62,9 +62,13 @@ final class Table
     {
         @Override
         public int compare(
+            @Nullable
             final IComponent component1,
+            @Nullable
             final IComponent component2 )
         {
+            assert component1 != null;
+            assert component2 != null;
             assert component1.getTableEnvironment().getLock().isHeldByCurrentThread();
             final ComponentPath componentPath1 = component1.getPath();
             assert componentPath1 != null;

main/table/org.gamegineer.table.ui.impl.tests/src/org/gamegineer/table/internal/ui/impl/util/swing/SortedListModelTest.java
@@ -1,6 +1,6 @@
 /*
  * SortedListModelTest.java
- * Copyright 2008-2014 Gamegineer contributors and others.
+ * Copyright 2008-2015 Gamegineer contributors and others.
  * All rights reserved.
  *
  * This program is free software: you can redistribute it and/or modify
@@ -25,6 +25,7 @@ import static org.junit.Assert.assertEquals;
 import java.util.Comparator;
 import javax.swing.DefaultListModel;
 import net.jcip.annotations.Immutable;
+import org.eclipse.jdt.annotation.Nullable;
 import org.junit.Test;
 
 /**
@@ -70,9 +71,14 @@ public final class SortedListModelTest
         {
             @Override
             public int compare(
+                @Nullable
                 final FakeElement o1,
+                @Nullable
                 final FakeElement o2 )
             {
+                assert o1 != null;
+                assert o2 != null;
+
                 return o1.value - o2.value;
             }
         } );

main/table/org.gamegineer.table.ui.impl/src/org/gamegineer/table/internal/ui/impl/databinding/conversion/Converters.java
@@ -1,6 +1,6 @@
 /*
  * Converters.java
- * Copyright 2008-2014 Gamegineer contributors and others.
+ * Copyright 2008-2015 Gamegineer contributors and others.
  * All rights reserved.
  *
  * This program is free software: you can redistribute it and/or modify
@@ -119,7 +119,6 @@ public final class Converters
         final IConverter converter,
         final String exceptionMessage )
     {
-        @SuppressWarnings( "null" )
         final IConverter decorator = new Converter( converter.getFromType(), converter.getToType() )
         {
             @Nullable

main/table/org.gamegineer.table.ui.impl/src/org/gamegineer/table/internal/ui/impl/model/FileHistoryPreferences.java
@@ -1,6 +1,6 @@
 /*
  * FileHistoryPreferences.java
- * Copyright 2008-2014 Gamegineer contributors and others.
+ * Copyright 2008-2015 Gamegineer contributors and others.
  * All rights reserved.
  *
  * This program is free software: you can redistribute it and/or modify
@@ -28,6 +28,7 @@ import java.util.List;
 import java.util.Map;
 import net.jcip.annotations.GuardedBy;
 import net.jcip.annotations.ThreadSafe;
+import org.eclipse.jdt.annotation.Nullable;
 import org.osgi.service.prefs.Preferences;
 
 /**
@@ -54,6 +55,7 @@ public final class FileHistoryPreferences
 
         @Override
         protected boolean removeEldestEntry(
+            @Nullable
             @SuppressWarnings( "unused" )
             final Map.Entry<File, File> eldest )
         {

main/table/org.gamegineer.table.ui.impl/src/org/gamegineer/table/internal/ui/impl/model/TableModel.java
@@ -1,6 +1,6 @@
 /*
  * TableModel.java
- * Copyright 2008-2014 Gamegineer contributors and others.
+ * Copyright 2008-2015 Gamegineer contributors and others.
  * All rights reserved.
  *
  * This program is free software: you can redistribute it and/or modify
@@ -74,9 +74,13 @@ public final class TableModel
     {
         @Override
         public int compare(
+            @Nullable
             final ComponentModel componentModel1,
+            @Nullable
             final ComponentModel componentModel2 )
         {
+            assert componentModel1 != null;
+            assert componentModel2 != null;
             assert componentModel1.getLock().isHeldByCurrentThread();
 
             final ComponentPath componentPath1 = componentModel1.getPath();

commit e9b249420d2b6a4802d23a6bb8cae151d6313734

Fix test break caused by 66f182e.
It is expected that the `other` parameter to `Comparable.compareTo` may
be `null`.  Due to the nullity annotations, we must explicitly check for
`other` being `null` and throwing `NullPointerException` ourselves
rather than just letting the runtime throw it when we attempt to access
`other`.

main/table/org.gamegineer.table.core/src/org/gamegineer/table/core/ComponentPath.java
@@ -101,8 +101,11 @@ public final class ComponentPath
         @Nullable
         final ComponentPath other )
     {
-        assert other != null;
-
+        if( other == null )
+        {
+            throw new NullPointerException( "other" ); //$NON-NLS-1$
+        }
+        
         final int parentPathCompareResult = ComparableUtils.compareTo( parentPath_, other.parentPath_ );
         if( parentPathCompareResult != 0 )
         {

main/table/org.gamegineer.table.ui.impl/src/org/gamegineer/table/internal/ui/impl/util/swing/SortedListModel.java
@@ -396,7 +396,10 @@ public final class SortedListModel<E>
             @Nullable
             final Entry other )
         {
-            assert other != null;
+            if( other == null )
+            {
+                throw new NullPointerException( "other" ); //$NON-NLS-1$
+            }
 
             final Object element = unsortedListModel_.getElementAt( index_ );
             final Object otherElement = unsortedListModel_.getElementAt( other.getIndex() );