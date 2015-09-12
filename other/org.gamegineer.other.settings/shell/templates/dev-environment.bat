22c45e2c9ea6529cdac4f358167170b3e7b1167b
Steven Soloff <s_soloff@yahoo.com>
2015-09-12 17:39:26 -0400

    Update development environment shell template.
    - Upgrade to Ant 1.9.6 and Maven 3.3.3.

--- other/org.gamegineer.other.settings/shell/templates/dev-environment.bat
+++ other/org.gamegineer.other.settings/shell/templates/dev-environment.bat
@@ -23,10 +23,10 @@ REM
 REM
 REM Home directories of all build tools.
 REM
-SET ANT_HOME=C:\Program Files\apache-ant-1.9.2
+SET ANT_HOME=C:\Program Files\apache-ant-1.9.6
 SET ECLIPSE_HOME=C:\Program Files\eclipse-4.5.0
 SET JAVA_HOME=C:\Program Files\Java\jdk1.7.0_51
-SET MAVEN_HOME=C:\Program Files\apache-maven-3.1.1
+SET MAVEN_HOME=C:\Program Files\apache-maven-3.3.3
 
 REM
 REM Build tool options.
