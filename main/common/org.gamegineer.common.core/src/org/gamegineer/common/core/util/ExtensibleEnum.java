0f969cfd4143d05fb5f6859c7764b2a2959f0ba1
Steven Soloff <s_soloff@yahoo.com>
2015-09-15 22:28:12 -0400

    Add null annotations for `Collection.toArray`.

--- main/common/org.gamegineer.common.core/src/org/gamegineer/common/core/util/ExtensibleEnum.java
+++ main/common/org.gamegineer.common.core/src/org/gamegineer/common/core/util/ExtensibleEnum.java
@@ -1,6 +1,6 @@
 /*
  * ExtensibleEnum.java
- * Copyright 2008-2014 Gamegineer contributors and others.
+ * Copyright 2008-2015 Gamegineer contributors and others.
  * All rights reserved.
  *
  * This program is free software: you can redistribute it and/or modify
@@ -256,6 +256,6 @@ public abstract class ExtensibleEnum
 
         @SuppressWarnings( "unchecked" )
         final T[] array = (T[])Array.newInstance( type, values.size() );
-        return nonNull( values.toArray( array ) );
+        return values.toArray( array );
     }
 }
