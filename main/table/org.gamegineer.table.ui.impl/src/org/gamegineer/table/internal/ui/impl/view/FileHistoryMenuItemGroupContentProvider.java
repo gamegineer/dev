40c1d14f4bc7ad3bf608f68fa4bd30863574ceb9
Steven Soloff <s_soloff@yahoo.com>
2015-09-15 23:12:11 -0400

    Add null annotations for the `File` class.

--- main/table/org.gamegineer.table.ui.impl/src/org/gamegineer/table/internal/ui/impl/view/FileHistoryMenuItemGroupContentProvider.java
+++ main/table/org.gamegineer.table.ui.impl/src/org/gamegineer/table/internal/ui/impl/view/FileHistoryMenuItemGroupContentProvider.java
@@ -1,6 +1,6 @@
 /*
  * FileHistoryMenuItemGroupContentProvider.java
- * Copyright 2008-2014 Gamegineer contributors and others.
+ * Copyright 2008-2015 Gamegineer contributors and others.
  * All rights reserved.
  *
  * This program is free software: you can redistribute it and/or modify
@@ -21,7 +21,6 @@
 
 package org.gamegineer.table.internal.ui.impl.view;
 
-import static org.gamegineer.common.core.runtime.NullAnalysis.nonNull;
 import java.io.File;
 import java.util.ArrayList;
 import java.util.Collection;
@@ -91,7 +90,7 @@ final class FileHistoryMenuItemGroupContentProvider
         final Collection<MenuItemDescriptor> menuItemDescriptors = new ArrayList<>();
         for( int index = 0, size = files.size(); index < size; ++index )
         {
-            final String path = nonNull( files.get( index ).getAbsolutePath() );
+            final String path = files.get( index ).getAbsolutePath();
             final String fileName = fileNames.get( index );
             final String label;
             if( Collections.frequency( fileNames, fileName ) > 1 )
